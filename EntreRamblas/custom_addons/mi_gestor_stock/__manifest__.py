e65501413814f3dfa448a1b7beb184ef4e480c9c
UM-Ruben <ruben.fernandezg@um.es>
2026-09-17 13:17:09 +0200

    Versión: registrar corrección del catálogo TPV

--- EntreRamblas/custom_addons/mi_gestor_stock/__manifest__.py
+++ EntreRamblas/custom_addons/mi_gestor_stock/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Mi Gestor de Stock Personalizado",
-    "version": "18.0.6.0.0",
+    "version": "18.0.6.0.1",
     "summary": "Recepción, stock con avisos e informes para la floristería Entre Ramblas",
     "author": "aarm5719",
     "license": "LGPL-3",
